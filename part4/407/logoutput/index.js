45014076c056afb0589271918ec2413261374716
Mikael Järvinen <mikaeljarv@gmail.com>
2024-07-03 14:59:10 +0300

    Test deployment pipeline

--- part4/407/logoutput/index.js
+++ part4/407/logoutput/index.js
@@ -41,7 +41,7 @@ const server = http.createServer(async (req, res) => {
         let output = `file content: ${fileContent}.\n`;
         output += `env variable: MESSAGE=${process.env.MESSAGE}.\n`;
         output += `${(new Date()).toISOString()}: ${randomString}.\n`;
-        output += `Ping / Pongs: ${counter}.\nTest edit new`;
+        output += `Ping / Pongs: ${counter}.\n`;
 
         res.end(output);
     } catch (e) {
