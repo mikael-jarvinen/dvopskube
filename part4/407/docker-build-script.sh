70c82c2482b9ac608d00819c0350d020e56e0885
Mikael Järvinen <mikaeljarv@gmail.com>
2024-07-03 13:27:51 +0300

    Add WIP part4

---
+++ part4/407/docker-build-script.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+# Current directory name
+DIR_NAME=${PWD##*/}
+
+# Function to build and push Docker images
+build_and_push() {
+  local context_dir=$1
+  local dockerfile=$2
+  local image_suffix=$3
+
+  # If a Dockerfile is specified, use it; otherwise, default to Dockerfile in the context directory
+  local dockerfile_arg=""
+  if [ -n "$dockerfile" ]; then
+    dockerfile_arg="-f $dockerfile"
+  fi
+
+  # Build and push the Docker image
+  (
+    cd "$context_dir"
+    docker build $dockerfile_arg -t usermine12/dvopskube-$DIR_NAME-$image_suffix .
+    docker push usermine12/dvopskube-$DIR_NAME-$image_suffix
+  ) &
+}
+
+# Build the Docker images for BE, FE, CRONJOB, and broadcaster in parallel
+build_and_push "logoutput" "" "logoutput"
+build_and_push "pingpong" "" "pingpong"
+
+# Wait for all background processes to finish
+wait
+
+echo "Docker images from $DIR_NAME have been built and pushed to Docker Hub."
---
+++ part4/407/pingpong/index.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const { PrismaClient } = require('@prisma/client');
+const subProcess = require('child_process');
+
+const prisma = new PrismaClient();
+
+let pushed = false;
+let connected = false;
+
+const server = http.createServer(async (req, res) => {
+    if (req.url === '/counter') {
+        const counter = await prisma.ping.count();
+
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(counter.toString());
+    } else if (req.url === '/healthcheck') {
+        try {
+            if (!connected) {
+                await prisma.$connect();
+                connected = true;
+            }
+
+            if (!pushed) {
+                // When the connection is successful push the DB schema
+                subProcess.execSync('npx prisma db push');
+                pushed = true;
+            }
+            
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('ok');
+        } catch (e) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('DB connection failed');
+        }
+    } else {
+        await prisma.ping.create();
+
+        const counter = await prisma.ping.count();
+
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`pong ${counter}`);
+    }
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port);
+
+console.log(`Server running on port ${port}`);
